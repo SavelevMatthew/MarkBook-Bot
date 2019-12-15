1058bd4b9714b1333e45e00d6c7522bca4fcb117
tramakarov <e2042912@gmail.com>
2019-12-16 03:02:03 +0500

    Improve support bug

--- src/main/java/main/BotCommands.java
+++ src/main/java/main/BotCommands.java
@@ -36,7 +36,7 @@ class BotCommands {
         msg.setParseMode(ParseMode.MARKDOWN);
         msg.setText(String.format("***\uD83D\uDED1 Проблема у @%s:***\n", username) + user.msg_text);
         msg.setChatId((long)409216737);
-        String[] splitted = user.properties.split("|");
+        String[] splitted = user.properties.split("!");
         user.status = UserStatus.valueOf(splitted[0]);
         user.properties = splitted[1];
         System.out.print(user.properties);
@@ -48,7 +48,7 @@ class BotCommands {
         SendMessage msg = new SendMessage();
         msg.setChatId((long)user.userId);
         msg.setText("Отправь текст запроса в службу поддержки");
-        user.properties = user.status.toString() + '|' + user.properties;
+        user.properties = user.status.toString() + '!' + user.properties;
         user.status = UserStatus.GET_SUPPORT_TEXT;
         SQLCommands.UpdateUserInfo(user);
         return msg;
